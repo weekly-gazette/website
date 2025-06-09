6f5415df1b58ecb552321565d950ad3026deb03b
Sequoyah Sudler <sequoyahsudler@Sequoyahs-MacBook-Pro-2.local>
2025-06-09 22:16:21 +0300

    First commit
    Signed-off-by: Sequoyah Sudler <sequoyahsudler@Sequoyahs-MacBook-Pro-2.local>

--- app/page.js
+++ app/page.js
@@ -1,103 +1,60 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const articles = ([
+    {
+      title: "Jews in Syria",
+      link: "https://weeklygazette.substack.com/p/jews-in-syria"
+    },
+    {
+      title: "The Catch-22 for Syrian Refugees in Azraq, Jordan",
+      link: "https://weeklygazette.substack.com/p/the-catch-22-for-syrian-refugees",
+    },
+    {
+      title: "Seven Days in Palestine",
+      link: "https://weeklygazette.substack.com/p/seven-days-in-palestine",
+    },
+    {
+      title: "The Cost of War",
+      link: "https://weeklygazette.substack.com/p/the-cost-of-war",
+    },
+  ]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  return (
+      <div className="flex flex-col p-5 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-y-5">
+          <section>
+            <header className="font-bold text-xl">The Weekly Gazette</header>
+            <header className="text-xs">In-depth journalism about any story worth telling</header>
+          </section>
+          <section>
+            <header className="font-bold pb-1 text-sm">Articles and essays</header>
+            <ol className="text-xs">
+              {articles.map(({title, link}) => (
+                <li key={title}>
+                  <a className="hover:text-orange-300" href={link}>{title}</a>
+                </li>
+              ))}
+            </ol>
+          </section>
+          <section>
+            <header className="font-bold pb-1 text-sm">Interactive reporting</header>
+            <p className="italic text-xs">Coming soon</p>
+          </section>
+        </main>
+        <footer className="pt-5 flex flex-col gap-y-1">
+          <header className="font-bold text-sm">About us</header>
+          <ol className="text-xs">
+            <li>
+              <a className="hover:text-orange-300" href="/">
+                The team
+              </a>
+            </li>
+            <li>
+              <a className="hover:text-orange-300" href="/">
+                Support our journalism
+              </a>
+            </li>
+          </ol>
+        </footer>
+      </div>
   );
 }
