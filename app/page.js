2f39efa2691d2c4ef1b24e73747f64c3328df248
ssudler <ssudler21@gmail.com>
2025-06-10 02:01:20 +0300

    Fix styling
    Signed-off-by: ssudler <ssudler21@gmail.com>

--- app/page.js
+++ app/page.js
@@ -19,42 +19,42 @@ export default function Home() {
   ]);
 
   return (
-      <div className="flex flex-col p-5 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-y-5">
+      <div className="flex flex-col p-10 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-y-10">
           <section>
-            <header className="font-bold text-xl">The Weekly Gazette</header>
-            <header className="text-xs">In-depth journalism about any story worth telling</header>
+            <header className="font-bold text-5xl">The Weekly Gazette</header>
+            <header className="text-xl pt-1">In-depth journalism about any story worth telling</header>
           </section>
           <section>
-            <header className="font-bold pb-1 text-sm">Articles and essays</header>
-            <ol className="text-xs">
+            <header className="font-bold pb-1 text-3xl">Articles and essays</header>
+            <ol className="text-xl">
               {articles.map(({title, link}) => (
-                <li key={title}>
-                  <a className="hover:text-orange-300" href={link}>{title}</a>
-                </li>
+                  <li key={title}>
+                    <a className="hover:text-orange-300" href={link}>{title}</a>
+                  </li>
               ))}
             </ol>
           </section>
           <section>
-            <header className="font-bold pb-1 text-sm">Interactive reporting</header>
-            <p className="italic text-xs">Coming soon</p>
+            <header className="font-bold pb-1 text-3xl">Interactive reporting</header>
+            <p className="italic text-xl">Coming soon</p>
+          </section>
+          <section>
+            <header className="font-bold text-3xl">About us</header>
+            <ol className="text-xl">
+              <li>
+                <a className="hover:text-orange-300" href="https://weeklygazette.substack.com/about">
+                  The team
+                </a>
+              </li>
+              <li>
+                <a className="hover:text-orange-300" href="https://weeklygazette.substack.com/about">
+                  Support our journalism
+                </a>
+              </li>
+            </ol>
           </section>
         </main>
-        <footer className="pt-5 flex flex-col gap-y-1">
-          <header className="font-bold text-sm">About us</header>
-          <ol className="text-xs">
-            <li>
-              <a className="hover:text-orange-300" href="https://weeklygazette.substack.com/about">
-                The team
-              </a>
-            </li>
-            <li>
-              <a className="hover:text-orange-300" href="https://weeklygazette.substack.com/about">
-                Support our journalism
-              </a>
-            </li>
-          </ol>
-        </footer>
       </div>
   );
 }
